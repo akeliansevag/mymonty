56d0f9132bce0eae804cb7eb9d38d5b42591a204
akeliansevag <akelian.sevag@gmail.com>
2023-11-24 16:55:40 +0200

    Initial

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: '1rem'
+    },
+    extend: {
+      screens: {
+        '2xl': '1280px'
+      },
+      colors: {
+        primary: colors.neutral[900],
+        secondary: colors.yellow[600]
+      }
+    }
+  },
+  plugins: [],
+}
