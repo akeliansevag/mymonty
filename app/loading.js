d5b0d8985896bfe4da7efa6398600232164f47ee
akeliansevag <akelian.sevag@gmail.com>
2023-11-27 12:24:18 +0200

    added loading

---
+++ app/loading.js
@@ -0,0 +1,7 @@
+export default function Loading() {
+    return (
+        <div className="fixed h-screen w-screen bg-yellow-600 text-white z-50 flex items-center justify-center">
+            <h1 className="text-[60px]">LOADING...</h1>
+        </div>
+    )
+}
