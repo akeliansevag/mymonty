d09e8af593eba7e0e042aa12d43e03ac52a5a61f
akeliansevag <akelian.sevag@gmail.com>
2023-11-27 09:56:26 +0200

    ,

--- app/page.js
+++ app/page.js
@@ -1,5 +1,4 @@
 import Banner from './components/homepage/Banner';
-import Header from './components/Header';
 import styles from './page.module.css';
 import SectionTwo from './components/homepage/SectionTwo';
 import SectionThree from './components/homepage/SectionThree';
@@ -7,12 +6,15 @@ import SectionFour from './components/homepage/SectionFour';
 import SectionFive from './components/homepage/SectionFive';
 import SectionSix from './components/homepage/SectionSix';
 import SectionSeven from './components/homepage/SectionSeven';
+import { Suspense } from 'react';
 
 export default function Home() {
   return (
     <main>
-      {/* <Header /> */}
-      <Banner />
+      <Suspense fallback={(<div>Loading...</div>)}>
+        <Banner />
+      </Suspense>
+
       <SectionTwo />
       <SectionThree />
       <SectionFour />
