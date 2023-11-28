73912556b69851e5abf993fdb5504f4899ee77f8
akeliansevag <akelian.sevag@gmail.com>
2023-11-28 13:09:18 +0200

    added new girl image to the mobile

--- app/components/homepage/Banner.js
+++ app/components/homepage/Banner.js
@@ -1,5 +1,6 @@
 import bannerImage from '@/public/home-banner.webp'
 import bannerElement from '@/public/bannerElement.webp';
+import girl from '@/public/girl.webp';
 import logo from '@/public/mymontylogo.svg';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -8,15 +9,15 @@ import React from 'react';
 
 const Banner = () => {
     return (
-        <section className='min-h-screen w-full relative pt-10 flex flex-col'>
-            <Image alt='Banner Image' fill placeholder='blur' quality={100} sizes='100vw' src={bannerImage} className='object-cover object-[70%] md:object-center -z-10'
+        <section className='min-h-screen w-full relative pt-10 flex flex-col bg-[#DCF4FE] md:bg-transparent'>
+            <Image alt='Banner Image' fill placeholder='blur' quality={100} sizes='100vw' src={bannerImage} className='object-cover custom-object-position md:object-center -z-10'
                 priority
             />
             <div className='container'>
                 <Image alt='Logo' src={logo} priority />
             </div>
-            <div className='container md:flex md:flex-1 md:gap-10'>
-                <div className='flex-[2] self-center pb-10'>
+            <div className='container p-0 flex flex-col flex-1 md:flex-row md:gap-10'>
+                <div className='p-4 md:flex-[2] self-center pb-10'>
                     <div>
                         <h1 className='font-black mt-4 md:mt-0 text-3xl sm:text-5xl md:text-6xl lg:text-[5.4rem] leading-none uppercase'>MyMonty will always have your back.</h1>
                         <p className='text-xl mt-5'>Explore our innovative ways to manage your finances.</p>
@@ -24,14 +25,20 @@ const Banner = () => {
                     </div>
 
                 </div>
-                <div className='flex-1 flex items-end'>
+
+                <div className='flex-1 md:flex-1 flex items-end flex-col justify-end relative'>
                     <Image alt='Banner Image' src={bannerElement}
-                        className=''
+                        className='z-10 px-4 md:p-0'
+                        priority
+                    />
+                    <Image fill quality={100} sizes='100vw' alt='Girl Image' src={girl}
+                        className='md:hidden object-cover object-top'
                         priority
                     />
                 </div>
             </div>
-        </section>
+
+        </section >
     )
 }
 
