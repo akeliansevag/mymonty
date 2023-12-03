3e0e6baa7f0aebdb786f9bb92dfffda221c158ee
Sevag Akelian <akelian.sevag@gmail.com>
2023-12-03 10:56:01 +0200

    added keys for the map

--- app/components/header/Nav.js
+++ app/components/header/Nav.js
@@ -14,26 +14,26 @@ const Nav = ({ handleMenuOpen, menuOpen }) => {
     return (
         <nav className='text-[1rem]'>
             <ul className={`flex`}>
-                {data && data.map((items) => {
+                {data && data.map((items, index) => {
                     return (
-                        <li onMouseLeave={() => items.children ? handleMouseLeave() : false} onMouseEnter={() => items.children ? handleMouseEnter() : false} className='h-[var(--header-height)] flex items-center group'>
+                        <li key={index} onMouseLeave={() => items.children ? handleMouseLeave() : false} onMouseEnter={() => items.children ? handleMouseEnter() : false} className='h-[var(--header-height)] flex items-center group'>
                             <span className='transition-all py-[7px] px-[20px] rounded-full group-hover:bg-black group-hover:text-white group-hover:cursor-default'>
                                 {items?.url ? <Link onClick={() => handleMenuOpen(false)} href={items.url}>{items?.title}</Link> : items.title}
                             </span>
                             {items.children && (
                                 <div className={`${menuOpen ? 'group-hover:block' : null} py-10 bg-[#f7f7f7] w-full absolute top-full left-0 hidden`}>
                                     <div className='container flex gap-10'>
-                                        {items.children.map((child) => {
+                                        {items.children.map((child, index2) => {
                                             return (
-                                                <div>
+                                                <div key={index2}>
                                                     <span className='font-bold block mb-[15px] text-xl'>
                                                         {child?.url ? <Link onClick={() => handleMenuOpen(false)} href={child.url}>{child.title}</Link> : child.title}
                                                     </span>
                                                     {child?.children && (
                                                         <ul>
-                                                            {child.children.map((c) => {
+                                                            {child.children.map((c, index3) => {
                                                                 return (
-                                                                    <li>
+                                                                    <li key={index3}>
                                                                         {c?.url ? <Link onClick={() => handleMenuOpen(false)} href={c.url}>{c?.title}</Link> : c.title}
                                                                     </li>
 
