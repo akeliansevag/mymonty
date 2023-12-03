8abb3c8090f719c0c7e66f1a070190bc804239cf
Sevag Akelian <akelian.sevag@gmail.com>
2023-12-03 23:53:52 +0200

    faq

---
+++ app/faq/data.js
@@ -0,0 +1,33 @@
+export const faqData = [
+    {
+        title: 'Advanced Salary',
+        children: [
+            {
+                title: 'What is an Advanced salary?',
+                description: 'An Advanced salary, offered by MyMonty, is a feature that allows our customers to access a portion of their salaries in advance'
+            },
+            {
+                title: 'How do I apply for an Advanced Salary?',
+                description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+            }
+        ],
+    },
+    {
+        title: 'Cardless ATM Deposits and Withdrawals',
+        children: [
+            {
+                title: 'How can I make a cardless ATM deposit using a QR code via MyMonty app?',
+                description: 'An Advanced salary, offered by MyMonty, is a feature that allows our customers to access a portion of their salaries in advance'
+            },
+            {
+                title: 'What happens if I want to add more cash during a cardless QR deposit?',
+                description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+            },
+            {
+                title: 'Can I cancel a cardless QR deposit after inserting the bank notes?',
+                description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'
+            },
+
+        ],
+    }
+];
---
+++ app/faq/page.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import Banner from '@/app/components/Banner';
+import banner from '@/public/faq-banner.png'
+import { websiteName } from '@/app/config';
+import Section from '../components/Section';
+import { faqData } from './data';
+import Accordion from './Accordion';
+
+export const metadata = {
+    title: websiteName + 'FAQ',
+    description: '',
+};
+
+const faq = () => {
+    const data = {
+        title: 'FAQ',
+        subtitle: 'FAQ',
+        desc: 'Our team is always ready to assist you or answer your inquiries. Fill out this form to reach us.',
+        image: {
+            layout: 'half',
+            url: banner,
+        },
+        cta: {
+            type: 'link',
+            link: {
+                name: 'Contact Us',
+                url: '/contact-us'
+            }
+        }
+    };
+
+
+    return (
+        <>
+            <Banner data={data} />
+            <Section>
+                <div className='container'>
+                    {faqData && (
+                        faqData.map((item, index) => {
+                            return (
+                                <Accordion key={index} items={item.children} title={item.title} />
+                            )
+                        })
+                    )}
+                </div>
+            </Section>
+        </>
+    )
+}
+
+export default faq;
