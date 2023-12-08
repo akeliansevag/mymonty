67d9adaa824db3ebfd43f30d4c2966f96817edb6
akeliansevag <akelian.sevag@gmail.com>
2023-12-08 16:09:08 +0200

    .

--- app/cards/CardWithdrawal.js
+++ app/cards/CardWithdrawal.js
@@ -14,22 +14,27 @@ import hand4 from '@/public/withdrawal/hand-4.webp';
 
 const CardWithdrawal = () => {
     const [imageIndex, setImageIndex] = useState(0);
+    const [darkTheme, setDarkTheme] = useState(false);
     const data = [
         {
             bg: bg1,
-            hand: hand1
+            hand: hand1,
+            dark: false
         },
         {
             bg: bg2,
-            hand: hand2
+            hand: hand2,
+            dark: false
         },
         {
             bg: bg3,
-            hand: hand3
+            hand: hand3,
+            dark: false
         },
         {
             bg: bg4,
-            hand: hand4
+            hand: hand4,
+            dark: true
         },
     ];
     useEffect(() => {
@@ -37,6 +42,7 @@ const CardWithdrawal = () => {
         let key = 0;
         setInterval(() => {
             if (key < data.length) {
+                data[key]?.dark ? setDarkTheme(true) : setDarkTheme(false);
                 setImageIndex(key);
                 key++;
                 if (key == data.length) {
@@ -46,9 +52,9 @@ const CardWithdrawal = () => {
         }, 750);
     }, [])
     return (
-        <div className='relative py-14 md:py-24'>
+        <div className={`relative py-14 md:py-24 ${!darkTheme ? 'text-white' : ''}`}>
             <div className='container text-center'>
-                <TextBlock center={true} title="Withdraw Whenever, Wherever" description="Get cash when you need it. Use your prepaid Mastercard to withdraw from ATMs anywhere in the world with no hidden fees." button={true} />
+                <TextBlock buttonColor={!darkTheme ? 'white' : ''} center={true} title="Withdraw Whenever, Wherever" description="Get cash when you need it. Use your prepaid Mastercard to withdraw from ATMs anywhere in the world with no hidden fees." button={true} />
             </div>
             {data && data.map((item, index) => {
                 return (
@@ -58,7 +64,7 @@ const CardWithdrawal = () => {
 
             {data && data.map((item, index2) => {
                 return (
-                    <Image priority key={index2} className={`${index2 !== imageIndex ? 'hidden' : ''} mt-20 md:w-1/2`} src={item.hand} alt='Card Hand' />
+                    <Image priority key={index2} className={`${index2 !== imageIndex ? 'hidden' : ''} mt-20 md:w-1/2 translate-x-[-22%] md:translate-x-0`} src={item.hand} alt='Card Hand' />
                 )
             })}
 
