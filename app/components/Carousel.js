436ec8055b417122a2d4df6d1f4ec432da1afa73
akeliansevag <akelian.sevag@gmail.com>
2023-12-12 14:31:20 +0200

    ,

--- app/components/Carousel.js
+++ app/components/Carousel.js
@@ -6,24 +6,61 @@ import Link from 'next/link';
 import { useAppContext } from '../AppContext';
 
 const Carousel = ({ data, video, videoMP4URL, videoWEBMURL, center }) => {
-    const [selectedIndex, setSelectedIndex] = useState(0);
+    const [selectedIndex, setSelectedIndex] = useState(null);
     const [filterClicked, setFilterClicked] = useState(false);
+    const [counter, setCounter] = useState(0);
+    const [timeoutID, setTimeoutID] = useState(null);
     const { handleOpenModal } = useAppContext();
 
     const handleSelect = (index) => {
         setSelectedIndex(index);
         setFilterClicked(true);
+        setCounter(0);
+    };
+    const duration = 8;
+    const changeSlide = () => {
+
+        setSelectedIndex((prev) => {
+            if ((prev + 1) < data.content.length) {
+                countUp();
+                return prev + 1;
+            } else {
+                setSelectedIndex(0);
+                setCounter(0);
+                countUp();
+                return prev;
+            }
+        });
+    }
+    const countUp = () => {
+        for (let j = 0; j <= duration; j++) {
+            let tid = setTimeout(() => {
+                setCounter(j);
+            }, 1000 * j);
+            setTimeoutID(tid);
+        }
+    };
+
+    const stopCounter = () => {
+        clearTimeout(timeoutID);
     };
 
     useEffect(() => {
         if (!filterClicked) {
-            let i = 1;
-            // setInterval(() => {
-            //     if (i < data.content.length) {
-            //         setSelectedIndex(i++);
-            //     }
-            // }, 1000);
+            countUp();
+            let interval = setInterval(changeSlide, duration * 1000);
+
+            return () => {
+                clearInterval(interval);
+                setCounter(0);
+            }
+        } else {
+            setCounter(0);
+            stopCounter();
         }
+    }, [filterClicked])
+    useEffect(() => {
+        setSelectedIndex(0);
     }, [])
     return (
         <section className={`${center ? 'flex flex-col justify-center' : ''} relative  min-h-[calc(100vh-(var(--mobile-header-height)))]`
@@ -48,6 +85,7 @@ const Carousel = ({ data, video, videoMP4URL, videoWEBMURL, center }) => {
                             <div key={index} className={`absolute py-14 lg:py-28 transition-opacity ease-in duration-[400ms] h-full w-full ${contentClasses}`}>
                                 <div className='container gap-10 flex flex-col h-full overflow-hidden text-center relative'>
                                     <div>
+                                        <h2>{counter}</h2>
                                         <TextBlock center={true} textColor="white" title={item.title} description={item.description} button={false} />
                                         {item.button && !item?.button?.modal && (
                                             <Link className='mm-button white mt-4' href={item.button?.url}>{item.button.text}</Link>
@@ -84,12 +122,16 @@ const Carousel = ({ data, video, videoMP4URL, videoWEBMURL, center }) => {
                 <div className='container justify-left flex gap-5 md:justify-center'>
                     {data && data?.filters.map((filter, index2) => {
                         let buttonClasses = index2 === selectedIndex ? ' !bg-[#2657D4] !border-[#2657D4]' : '';
+                        let widthClass = (index2 === selectedIndex && !filterClicked) ? '!w-full transition-all' : 'transition-none w-0';
                         return (
-                            <div onClick={() => handleSelect(index2)} key={index2} className={` whitespace-nowrap mm-button default hover:bg-[#2657D4] hover:border-[#2657D4] cursor-pointer ${buttonClasses}`}>
-                                {filter.title}
+                            <div onClick={() => handleSelect(index2)} key={index2} className={`whitespace-nowrap mm-button default hover:bg-[#2657D4] hover:border-[#2657D4] cursor-pointer ${buttonClasses} relative overflow-hidden z-1`}>
+                                <div className={`bg-[#1a3e96] w-0 opacity-80 absolute h-full top-0 left-0 duration-[${duration}s] ease-linear ${widthClass}`}></div>
+                                <div className='z-10 block relative'>
+                                    {filter.title}
+                                </div>
+
                             </div>
                         )
-
                     })
                     }
                 </div>
