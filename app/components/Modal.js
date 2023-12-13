0481bc229e63282cf238ba0bcf4152bdb10dc946
Hassan Jammal <hassan-jammal@hotmail.com>
2023-12-13 12:11:56 +0200

    Auto stash before merge of "main" and "origin/main"

--- app/components/Modal.js
+++ app/components/Modal.js
@@ -2,25 +2,25 @@
 import { useEffect, useRef, useState, Fragment } from 'react';
 import { Dialog, Transition } from '@headlessui/react'
 import Link from 'next/link';
-import { countries } from '@/app/countries';
 
 const Modal = ({ isOpen, handleOpenModal, handleCloseModal }) => {
 
   const apiUrl = process.env.apiUrl;
 
   const [openCountry, setOpenCountry] = useState(false);
-  const [userInfo, setUserInfo] = useState({});
+  const [countries, setCountries] = useState([]);
+
+  const [searchTerm, setSearchTerm] = useState('');
+  const [selectedCountry, setSelectedCountry] = useState(null);
+
   const cancelButtonRef = useRef(null);
   const dropdownRef = useRef(null);
 
-  const [searchInput, setSearchInput] = useState('');
-  const [selectedCountry, setSelectedCountry] = useState(null);
-  const [selectedCountryCode, setSelectedCountryCode] = useState('');
-
   const [formData, setFormData] = useState({
-    country_id: '',
-    code: '',
+    code: selectedCountry ? selectedCountry.code : '', // Default code value based on selected country
+    country_id: selectedCountry ? selectedCountry.id : '', // Default country_id value based on selected country
     mobile: '',
+    phone_number: '',
   });
 
   const [errors, setErrors] = useState({});
@@ -43,24 +43,6 @@ const Modal = ({ isOpen, handleOpenModal, handleCloseModal }) => {
     return isValid;
   };
 
-  const handleSearchChange = (e) => setSearchInput(e.target.value);
-
-  const handleCountryClick = (country) => {
-    setOpenCountry(!openCountry);
-    setSearchInput('');
-    setSelectedCountry(country);
-
-    setFormData((prevData) => ({
-      ...prevData,
-      code: country.code,
-      country_id: country.id,
-    }));
-  };
-
-  const filteredCountries = countries.filter((country) =>
-    country.name.toLowerCase().includes(searchInput.toLowerCase())
-  );
-
   useEffect(() => {
     const handleClickOutside = (event) => {
       if (
@@ -69,7 +51,7 @@ const Modal = ({ isOpen, handleOpenModal, handleCloseModal }) => {
         !document.getElementById('calling_code').contains(event.target)
       ) {
         setOpenCountry(false);
-        setSearchInput('');
+        setSearchTerm('')
       }
     };
 
@@ -87,10 +69,22 @@ const Modal = ({ isOpen, handleOpenModal, handleCloseModal }) => {
       return;
     }
 
-    setFormData((prevData) => ({
-      ...prevData,
-      [id]: value,
-    }));
+    setFormData((prevData) => {
+      let updatedData = {
+        ...prevData,
+        [id]: value,
+      };
+
+      // If the changed field is 'mobile', update 'phone_number'
+      if (id === 'mobile') {
+        updatedData = {
+          ...updatedData,
+          phone_number: `+${prevData.code}${value}`,
+        };
+      }
+
+      return updatedData;
+    });
 
     setErrors((prevErrors) => ({
       ...prevErrors,
@@ -116,31 +110,27 @@ const Modal = ({ isOpen, handleOpenModal, handleCloseModal }) => {
         const data = await response.json();
 
         if (response.ok) {
-          const countryId = countries.find((c) => c.code === userInfo.calling_code)?.id;
-
           setSubmissionStatus('success');
           setTimeout(() => {
             handleCloseModal();
 
             setFormData({
-              country_id: countryId,
-              code: userInfo.calling_code,
+              code: selectedCountry ? selectedCountry.code : '',
+              country_id: selectedCountry ? selectedCountry.id : '',
               mobile: '',
+              phone_number: '',
             });
             setErrors({});
             setSubmissionStatus(null);
           }, 2000);
         } else {
           setSubmissionStatus('error');
-          if (data.code === '23000') {
-            setErrors({ mobile: 'The mobile you entered is already registered.' } || {});
-          } else {
-            setErrors(data.data || {});
-          }
+          setErrors(data.data || {});
         }
       } catch (error) {
         setSubmissionStatus('error');
-        setErrors({ message: 'An error occurred while submitting the form.' });
+        setErrors(error || {});
+        // setErrors({ message: 'An error occurred while submitting the form.' });
       } finally {
         setLoading(false);
       }
@@ -150,30 +140,57 @@ const Modal = ({ isOpen, handleOpenModal, handleCloseModal }) => {
   };
 
   useEffect(() => {
-    const fetchUserInfo = async () => {
+    const fetchData = async () => {
       try {
-        const response = await fetch(`${apiUrl}/user-ip`);
-        if (response.ok) {
-          const data = await response.json();
-          setUserInfo(data);
+        // const countriesResponse = await fetch(`${apiUrl}/get-countries`);
+        const userResponse = await fetch(`${apiUrl}/user-ip`);
 
-          const countryId = countries.find((c) => c.code === data.calling_code)?.id;
+        if (userResponse.ok) {
+          const userData = await userResponse.json();
 
-          setFormData((prevData) => ({
-            ...prevData,
-            code: data.calling_code || '',
-            country_id: countryId,
-          }));
-        } else {
-          console.error('Error fetching user information:', response.status);
+          const countriesList = userData.countries
+          const countriesMap = new Map(countriesList.map(country => [country.iso2, country]));
+          setCountries(countriesList);
+
+          const userCountryIso2 = userData?.iso2;
+          const userCountry = countriesMap.get(userCountryIso2);
+
+          if (userCountry) {
+            setSelectedCountry(userCountry);
+
+            setFormData(prevFormData => ({
+              ...prevFormData,
+              code: userCountry.code,
+              country_id: userCountry.id,
+            }));
+          }
         }
       } catch (error) {
-        console.error('Error fetching user information:', error);
+        console.error('Error fetching data:', error);
       }
     };
 
-    fetchUserInfo();
+    fetchData();
   }, []);
+
+
+  const handleCountryClick = (country) => {
+    setSelectedCountry(country);
+
+    // Update formData values when a country is selected
+    setFormData(prevFormData => ({
+      ...prevFormData,
+      code: country.code,
+      country_id: country.id,
+    }));
+
+    setOpenCountry(!openCountry);
+    setSearchTerm('');
+  };
+
+  const filteredCountries = countries.filter(country =>
+    country.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
     <Transition.Root show={isOpen} as={Fragment}>
@@ -222,22 +239,15 @@ const Modal = ({ isOpen, handleOpenModal, handleCloseModal }) => {
                                 <div className="flex w-full">
                                   <div className="calling_code">
                                     <div className="flex h-full justify-between items-center">
-                                      <div id="calling_code" onClick={(e) => { setOpenCountry(!openCountry) }} className="select bg-gray-100 border-gray-300 border-2 border-e-0 py-2.5 pl-5 pr-2 rounded-xl rounded-e-none flex justify-between items-center text-gray-400 text-base">
-                                        {selectedCountry ? (
+                                      <div id="calling_code" onClick={() => setOpenCountry(!openCountry)} className="select bg-gray-100 border-gray-300 border-2 border-e-0 py-2.5 pl-5 pr-2 rounded-xl rounded-e-none flex justify-between items-center text-gray-400 text-base">
+                                        {selectedCountry && (
                                           <>
                                             <div className="text-lg">
-                                              {selectedCountry.flag}
+                                              <img src={`/flags/${selectedCountry.iso2}.svg`} alt={selectedCountry.name} />
                                             </div>
                                             <div className='text-black'>
                                               +{selectedCountry.code}
                                             </div>
-                                          </>
-                                        ) : (
-                                          <>
-                                            <div className="text-lg">
-                                              {userInfo.country_flag_emoji}
-                                            </div>
-                                            <div className='text-black'>+{userInfo.calling_code}</div>
                                           </>
                                         )}
                                         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -249,13 +259,13 @@ const Modal = ({ isOpen, handleOpenModal, handleCloseModal }) => {
 
                                   <div id="calling_code-drop" className={`dropdown px-1 ${openCountry ? 'visible' : 'hidden'}`} ref={dropdownRef}>
                                     <div className="search-wrapper !sticky top-0 left-0 flex justify-between items-center">
-                                      <input type="text" id="search-country" placeholder="Search" className="search-country outline-0 w-full px-3.5 py-2.5 bg-gray-100 border-0 text-sm" value={searchInput} onChange={handleSearchChange} />
-                                      {searchInput && (
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" width="24" height="24" className='px-1 cursor-pointer' onClick={() => { setSearchInput(''); }}>
+                                      <input type="text" id="search-country" placeholder="Search" className="search-country outline-0 w-full px-3.5 py-2.5 bg-gray-100 border-0 text-sm" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+                                      {searchTerm && (
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" width="24" height="24" className='px-1 cursor-pointer' onClick={(e) => { setSearchTerm(''); }}>
                                           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
                                         </svg>
                                       )}
-                                      {searchInput === '' && (
+                                      {searchTerm === '' && (
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" width="24" height="24">
                                           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-5-5m2-5a8 8 0 1 0-16 0 8 8 0 0 0 16 0z"></path>
                                         </svg>
@@ -264,11 +274,11 @@ const Modal = ({ isOpen, handleOpenModal, handleCloseModal }) => {
 
                                     <ul id="country-ul">
                                       {filteredCountries.map((country, index) => (
-                                        <li key={index} className="my-1 rounded-md cursor-pointer px-2.5 hover:bg-gray-300" data-id={country.id} data-code={country.code} data-name={country.name} data-iso={country.iso2} onClick={() => handleCountryClick(country)}>
+                                        <li key={index} className="my-1 rounded-md cursor-pointer py-2 px-2.5 hover:bg-gray-300" onClick={() => handleCountryClick(country)}>
                                           <span className="flex items-center">
-                                            {country.flag}
-                                            <span className="ms-1 text-sm">{country.name}</span>
-                                            <span className="ms-1 text-sm">{country.code}</span>
+                                            <img src={`/flags/${country.iso2}.svg`} alt={country.name} />
+                                            <span className="ms-4 text-sm">{country.name}</span>
+                                            <span className="ms-4 text-sm">{country.code}</span>
                                           </span>
                                         </li>
                                       ))}
@@ -282,7 +292,6 @@ const Modal = ({ isOpen, handleOpenModal, handleCloseModal }) => {
                                     </div>
                                   </div>
 
-                                  <input type="hidden" name="code" value={selectedCountryCode} className="onboarding-code" />
                                   <input
                                     id="mobile"
                                     type="tel"
@@ -293,6 +302,7 @@ const Modal = ({ isOpen, handleOpenModal, handleCloseModal }) => {
                                     className={`focus:outline-none onboarding-mobile outline-0 w-full bg-gray-100 border-gray-300 border-2 border-s-0 pl-1 pr-3.5 py-2.5 rounded-xl rounded-s-none text-lg ${errors.mobile ? 'border-s-2 border-red-500 selection:' : ''}`} />
                                 </div>
                                 <span className='text-red-500 text-base'>{errors.mobile}</span>
+                                <span className='text-red-500 text-base'>{errors.phone_number}</span>
                               </div>
                             </div>
 
@@ -342,10 +352,10 @@ const Modal = ({ isOpen, handleOpenModal, handleCloseModal }) => {
                 </Dialog.Panel>
               </div>
             </Transition.Child>
-          </div>
-        </div>
-      </Dialog>
-    </Transition.Root>
+          </div >
+        </div >
+      </Dialog >
+    </Transition.Root >
   )
 }
 
