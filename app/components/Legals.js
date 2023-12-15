03d3aad5b7ea5ffca3d40755236e1f32e8814ce0
Hassan Jammal <hassan-jammal@hotmail.com>
2023-12-15 12:52:23 +0200

    .

--- app/components/Legals.js
+++ app/components/Legals.js
@@ -2,20 +2,18 @@
 import React from 'react';
 import Section from '../components/Section';
 import useFetch from '../api/useFetch';
-import { COOKIE_API } from '../api/api';
 
-const Cookie = () => {
-    const { data, loading, error } = useFetch(COOKIE_API);
-    console.log(data)
+const Legals = ({ api }) => {
+    const { data, loading, error } = useFetch(api);
     return (
         <Section>
             <div className='container'>
                 {data && (
                     data.data?.map((item, index) => {
                         return (
-                            <>
+                            <div key={index}>
                                 {item.description}
-                            </>
+                            </div>
                         )
                     })
                 )}
@@ -24,4 +22,4 @@ const Cookie = () => {
     )
 }
 
-export default Cookie;
+export default Legals;
--- app/cookie-policy/page.js
+++ app/cookie-policy/page.js
@@ -3,7 +3,8 @@ import Banner from '@/app/components/Banner';
 import { websiteName } from '@/app/config';
 import Section from '../components/Section';
 import styles from './page.module.css';
-import Cookie from './Cookie';
+import { COOKIE_API } from '../api/api';
+import Legals from '../components/Legals';
 
 export const metadata = {
     title: websiteName + 'Cookie Policy',
@@ -16,19 +17,13 @@ const cookie = () => {
         subtitle: 'Privacy Policy',
         desc: 'Continuous Development: At MyMonty, we are dedicated to nurturing the professional',
         image: false,
-        cta: {
-            type: 'sfsfsdf',
-            button: {
-                name: 'Download app'
-            }
-        }
     };
 
 
     return (
         <div className={styles.privacyPolicy}>
             <Banner data={data} />
-            <Cookie />
+            <Legals api={COOKIE_API} />
         </div>
     )
 }
