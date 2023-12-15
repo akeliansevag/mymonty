fb0fdf0fb885eeb983200ae62a5e8a289ef7413f
akeliansevag <akelian.sevag@gmail.com>
2023-12-15 12:04:04 +0200

    .

--- app/homepage/HomeBanner.js
+++ app/homepage/HomeBanner.js
@@ -8,7 +8,7 @@ import { useAppContext } from '@/app/AppContext';
 const HomeBanner = () => {
     const { handleOpenModal } = useAppContext();
     return (
-        <section className='flex flex-col relative min-h-screen pt-[var(--header-height)] overflow-hidden'>
+        <section className='flex flex-col relative min-h-[100dvh] pt-[var(--header-height)] overflow-hidden'>
             <div className='absolute w-full h-full top-0 left-0 -z-10 bg-[#d8f1fb]'>
                 <Image quality={100} sizes='100vw' fill priority placeholder='blur' alt='Girl Holding a phone' src={banner} className='custom-object-position md:object-center object-cover -z-9' />
             </div>
@@ -17,9 +17,9 @@ const HomeBanner = () => {
             <div className='flex-1 flex-col sm:flex-row container flex gap-20'>
                 <div className='flex-1 self-center sm:-mt-16'>
                     <h1 className='font-black text-4xl sm:text-5xl md:text-6xl lg:text-[5.4rem] leading-none uppercase'>
-                        MYMONTY BETTER THAN A BANK.
+                        BETTER THAN A BANK.
                     </h1>
-                    <p className='text-xl mt-8'>Explore our innovative ways to manage your finances.</p>
+                    <p className='text-xl mt-8'>Explore our innovative ways to manage your&nbsp;finances.</p>
                     <button onClick={handleOpenModal} className='mm-button mt-8 blue'>Get the app</button>
                 </div>
                 <div className='flex-1 flex items-end'>
