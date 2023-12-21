0ec38492fd351065446c8a795029819573fb07f8
Hassan Al Jammal <hassan-jammal@hotmail.com>
2023-12-21 13:01:44 +0200

    .

--- app/components/Legals.js
+++ app/components/Legals.js
@@ -6,17 +6,15 @@ import useFetch from '../api/useFetch';
 const Legals = ({ api }) => {
     const { data, loading, error } = useFetch(api);
     return (
-        <Section>
-            <div className='container'>
-                {data && (
-                    data.data?.map((item, index) => {
-                        return (
-                            <div key={index} className='text-base html-content' dangerouslySetInnerHTML={{ __html: item.description }} />
-                        )
-                    })
-                )}
-            </div>
-        </Section>
+        <div className='container py-10 lg:py-20 relative'>
+            {data && (
+                data.data?.map((item, index) => {
+                    return (
+                        <div key={index} className='text-base html-content' dangerouslySetInnerHTML={{ __html: item.description }} />
+                    )
+                })
+            )}
+        </div>
     )
 }
 
