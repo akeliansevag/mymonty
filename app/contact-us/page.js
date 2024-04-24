66cf99dd775dd83cb956d1e630166ab863ed834b
Hassan Al Jammal <hassan-jammal@hotmail.com>
2024-04-24 15:31:58 +0300

    .

--- app/contact-us/page.js
+++ app/contact-us/page.js
@@ -8,7 +8,7 @@ const ContactUs = () => {
   const data = {
     title: 'Do you have questions?',
     subtitle: 'Contact Us',
-    desc: 'Our team is always ready to assist you or answer your inquiries. Fill out this form to reach us.',
+    desc: 'You can contact us Monday to Friday from 9:00 AM to 5:00 PM on WhatsApp at +961 71 871 871 or send us an email at supportlb@mymonty.com. If you need assistance outside these hours, don\'t hesitate to contact our 24/7 Call Center at +961 1 734 020.',
     image: {
       inGrid: true,
       layout: 'half',
