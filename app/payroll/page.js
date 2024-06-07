7ef4d1b1e771cfff37f4ebf109b59353d9838e83
Hassan Al Jammal <hassan-jammal@hotmail.com>
2024-06-07 16:57:10 +0300

    .

--- app/payroll/page.js
+++ app/payroll/page.js
@@ -4,21 +4,8 @@ import payroll from '@/public/personal/contact-us.webp';
 import PayrollForm from './PayrollForm';
 
 const Payroll = () => {
-
-  const data = {
-    title: 'Do you have questions?',
-    subtitle: 'Payroll Us',
-    desc: 'You can contact us Monday to Friday from 9:00 AM to 5:00 PM on WhatsApp at +961 71 871 871 or send us an email at supportlb@mymonty.com. If you need assistance outside these hours, don\'t hesitate to contact our 24/7 Call Center at +961 1 734 020.',
-    image: {
-      inGrid: true,
-      layout: 'half',
-      url: payroll,
-    },
-  };
-
   return (
     <div>
-      <Banner data={data} />
       <PayrollForm />
     </div>
   );
