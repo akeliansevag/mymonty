b7ec6b6793cfa43a0d6244151c548290129fed25
akeliansevag <akelian.sevag@gmail.com>
2024-10-03 12:57:18 +0300

    .

--- app/zero-fees/page.js
+++ app/zero-fees/page.js
@@ -6,8 +6,11 @@ import mobileBanner from '@/public/fees-mobile.webp';
 const landing = () => {
   return (
     <section className='pt-[var(--header-height)]'>
-        <Image className='max-sm:hidden w-full' quality={100} sizes='100vw' priority placeholder='blur' alt='Fees Banner' src={banner} />
-        <Image className='sm:hidden w-full' quality={100} sizes='100vw' priority placeholder='blur' alt='Fees Banner' src={mobileBanner} />
+        <div className='container'>
+          <Image className='max-sm:hidden w-full' quality={100} sizes='100vw' priority placeholder='blur' alt='Fees Banner' src={banner} />
+          <Image className='sm:hidden w-full' quality={100} sizes='100vw' priority placeholder='blur' alt='Fees Banner' src={mobileBanner} />
+        </div>
+        
     </section>
   )
 }
