f233ca2ee0bc33ecf49c5d9ab2ff1d7a91d1492b
akeliansevag <akelian.sevag@gmail.com>
2024-10-03 12:13:29 +0300

    .

--- app/App.js
+++ app/App.js
@@ -8,10 +8,8 @@ import { Suspense } from 'react';
 import Modal from './components/Modal';
 import { useAppContext } from './AppContext';
 import Form from './components/Form';
-import Link from 'next/link';
-import Image from 'next/image';
-import feesMobile from '@/public/0fees-mobile.webp';
-import fees from '@/public/0fees-desktop.webp';
+import ZeroFees from './components/ZeroFees';
+import { usePathname } from 'next/navigation';
 
 
 
@@ -19,13 +17,13 @@ const App = ({ children }) => {
     const { handleOpenModal, handleCloseModal, openModal, setFormComponent, formComponent, largeWidth, setLargeWidth } = useAppContext();
 
     const dynamicFormComponent = <Form />;
-
+    const pathname = usePathname();
     return (
-        <>
-            <Link href="/zero-fees" className="fixed z-20 bottom-0 w-full">
-                <Image className='max-sm:hidden w-full' quality={100} sizes='100vw' priority placeholder='blur' alt='Fees Banner' src={fees} />
-                <Image className='sm:hidden w-full' quality={100} sizes='100vw' priority placeholder='blur' alt='Fees Banner' src={feesMobile} />
-            </Link>
+        <>  
+            {pathname != '/zero-fees' && (
+                <ZeroFees />
+            )}
+            
             <Modal formComponent={formComponent || dynamicFormComponent} isOpen={openModal} handleOpenModal={handleOpenModal} handleCloseModal={handleCloseModal} largeWidth={largeWidth} />
             <Header />
             <div>
