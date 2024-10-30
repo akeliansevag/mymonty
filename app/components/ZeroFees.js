1fe8d47c0696115c296cc1b88c32aacbf9302109
akeliansevag <akelian.sevag@gmail.com>
2024-10-30 15:14:01 +0200

    .

--- app/components/ZeroFees.js
+++ app/components/ZeroFees.js
@@ -2,8 +2,8 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import feesMobile from '@/public/0fees-mobile-2.webp';
-import fees from '@/public/0fees-desktop.webp';
+import feesMobile from '@/public/0fees-mobile-3.webp';
+import fees from '@/public/0fees-desktop-3.webp';
 
 
 const ZeroFees = () => {
