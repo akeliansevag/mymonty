edbe2ae3f38e3d616425dbb523c5bda7595f8af3
akeliansevag <akelian.sevag@gmail.com>
2024-11-25 15:23:07 +0200

    fixes

--- app/physical-platinum-mastercard/Icon.js
+++ app/physical-platinum-mastercard/Icon.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Icon = ({color,image,active,title}) => {
   return (
     <>
-      <img src={image} />
+      <img className="max-lg:w-[60px]" src={image} />
       <span style={{color: active ? color : 'black' ,fontWeight: active ? 'bold' : 'normal'}} className="max-md:mt-0 max-md:text-base mt-2">{title}</span>
     </>  
   )
