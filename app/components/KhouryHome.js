667ffab2ed8108422303e7e2db02d33ddde9bee2
akeliansevag <akelian.sevag@gmail.com>
2024-12-12 16:43:33 +0200

    changed khouryhome banner

--- app/components/KhouryHome.js
+++ app/components/KhouryHome.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import khouryhome from '@/public/khouryhomeaddesktop.webp';
+import khouryhome from '@/public/khouryhomedesktop2.webp';
 import khouryhomemobile from '@/public/khouryhomeadmobile.webp';
 
 
