2d0ed1c33ee13d1b2b2122537c1488b23f7b6c0a
Hassan Al Jammal <hassan-jammal@hotmail.com>
2025-02-28 15:41:21 +0200

    .

--- app/page.js
+++ app/page.js
@@ -19,8 +19,6 @@ export default function Home() {
       <HomeBanner />
       <SectionTwo />
       <SectionThree />
-      <SectionFour />
-      <Section />
       <SectionFive />
       <SectionSix />
     </main>
