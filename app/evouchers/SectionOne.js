ebf454be5a17e0ee3cd191c4030c718889052942
akeliansevag <akelian.sevag@gmail.com>
2025-03-26 15:37:24 +0200

    .

--- app/evouchers/SectionOne.js
+++ app/evouchers/SectionOne.js
@@ -6,27 +6,32 @@ const SectionOne = () => {
     {
         title: 'Game on',
         description: 'We’re hooking you up with all the top gaming vouchers. From <strong>PlayStation, Xbox</strong>, and <strong>Steam</strong> credits to in-game boosts for <strong>PUBG, Valorant, League of Legends</strong>, and many more, you’re always just a tap away from your next win.',
-        image: '/game-on.webp'
+        image: '/game-on.webp',
+        smallerImage: false
     },
     {
         title: 'Just Get That Gift',
         description: 'Whether your gifting others or yourself, choose from platforms like <strong>Google Play, iTunes, eBay, Amazon</strong> and more. The best gifts are the ones that are easy to give (and fun to receive).',
-        image: '/just-get-that-gift.webp'
+        image: '/just-get-that-gift.webp',
+        smallerImage: true
     },
     {
         title: 'Stream, Listen, and Chill',
         description: 'Leave your next binge-watch or playlist to us. Find vouchers for <strong>Netflix, Shahid, Spotify, Anghami, Amazon Prime Video, Twitch, or Hulu</strong>, right where you need them. Just keep the good times rolling.',
-        image: '/stream.webp'
+        image: '/stream.webp',
+        smallerImage: true
     },
     {
         title: 'Dress to Impress',
         description: 'Let the shopping spree begin! Get your hands on vouchers from popular retailers like <strong>SHEIN</strong> or <strong>Macy\'s</strong>, and shop the hottest trends.',
-        image: '/dress-to-impress.webp'
+        image: '/dress-to-impress.webp',
+        smallerImage: true
     },
     {
         title: 'Stay Connected, No Cables Attached!',
         description: 'MyMonty’s internet vouchers are taps away. Top up with <strong>Mobi, IDM, Sodetel</strong>, and more, straight from your MyMonty app. Staying connected is that easy.',
-        image: '/stay-connected.webp'
+        image: '/stay-connected.webp',
+        smallerImage: true
     },
   ];
   return (
@@ -35,12 +40,12 @@ const SectionOne = () => {
             <div className='flex flex-col lg:gap-24 max-lg:gap-14'>
                 {items.map((item,index)=>{
                     return (
-                        <div className={`flex max-lg:flex-col lg:items-center max-lg:gap-10 lg:gap-24 ${index%2!=0 ? 'lg:flex-row-reverse' : 'lg:flex-row'}`}>
+                        <div className={`flex max-lg:flex-col lg:justify-between lg:items-center max-lg:gap-10 lg:gap-24 ${index%2!=0 ? 'lg:flex-row-reverse' : 'lg:flex-row'}`}>
                             <div className='lg:w-1/2'>
                                 <TextBlock title={item.title} description={item.description} button={true} buttonColor="dark" />
                             </div>
-                            <div className='lg:w-1/2'>
-                                <img src={item.image} alt={item.title}/>
+                            <div className={`${item.smallerImage ? 'max-w-[500px]' : ''} lg:w-1/2`}>
+                                <img className='' src={item.image} alt={item.title}/>
                             </div>
                         </div>
                     )
