ef90d9934820e658ed026bcdf2b98f1cd7c5fbd7
akeliansevag <akelian.sevag@gmail.com>
2025-03-27 15:42:14 +0200

    .

---
+++ app/ihjoz/SectionFour.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import TextBlock from '../components/TextBlock';
+
+const SectionFour = () => {
+  return (
+    <section className='lg:py-24 max-lg:py-10 bg-white'>
+        <div className='container'>
+            <div className='flex items-center justify-between gap-10'>
+                <div className='lg:w-1/2'>
+                    <img src='/never-miss.webp' />
+                </div>
+                <div className='lg:w-1/2'>
+                    <TextBlock title="Never miss out on the fun with Ihjoz and Mymonty!" />
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default SectionFour;
---
+++ app/ihjoz/page.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { websiteName } from '@/app/config';
+import Banner from '@/app/components/Banner';
+import banner from '@/public/ihjoz-banner.webp';
+import SectionOne from './SectionOne';
+import SectionThree from './SectionThree';
+import SectionFour from './SectionFour';
+
+
+export const metadata = {
+    title: websiteName + 'Ihjoz',
+    description: '',
+};
+
+const accounts = () => {
+    const data = {
+        title: 'Ihjoz with MyMonty',
+        subtitle: 'Payments',
+        desc: 'With MyMonty, you\'ll never run out of things to do! Paying for your tickets on Ihjoz just got easier and quicker!',
+        image: {
+            inGrid: true,
+            layout: 'half',
+            url: banner,
+        },
+        cta: {
+            type: 'button',
+            button: {
+                name: 'Download App'
+            }
+        }
+    };
+    return (
+        <div>
+            <Banner data={data} />
+            <SectionOne />
+            <SectionThree />
+            <SectionFour />
+        </div>
+    )
+}
+
+export default accounts;
