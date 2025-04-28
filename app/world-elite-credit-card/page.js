66b1755f7b517824a2426f6832a021551337aabc
Hassan Al Jammal <hassan-jammal@hotmail.com>
2025-04-28 12:26:43 +0300

    Add file extensions to terms and conditions and KFS links in World Elite Credit Card component

--- app/world-elite-credit-card/page.js
+++ app/world-elite-credit-card/page.js
@@ -33,8 +33,8 @@ const worldEliteCards = () => {
     };
 
     const terms = {
-        termsAndConditions: 'credit-cards-terms-and-conditions-world-elite-credit-card-v1-15.04.2025', 
-        kfs: 'world-elite-kfs-v.01'
+        termsAndConditions: 'credit-cards-terms-and-conditions-world-elite-credit-card-v1-15.04.2025.pdf', 
+        kfs: 'world-elite-kfs-v.01.pdf'
     }
 
     return (
