89bab5b70073ae00ab05d8147e943e3a13582817
Hassan Al Jammal <hassan-jammal@hotmail.com>
2025-05-14 16:52:45 +0300

    Enhance CardsSlider with autoplay functionality and adjust Under18 component margin for improved layout

--- app/mymonty-under-18/CardsSlider.js
+++ app/mymonty-under-18/CardsSlider.js
@@ -1,11 +1,12 @@
 'use client';
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { EffectFade } from 'swiper/modules';
+import { EffectFade, Autoplay } from 'swiper/modules';
 
 import 'swiper/css';
 import 'swiper/css/keyboard';
 import 'swiper/css/effect-fade';
+import 'swiper/css/autoplay';
 
 import howToGetMyMonty1 from '@/public/how-to-get-mymonty-under-18-1.webp';
 import howToGetMyMonty2 from '@/public/how-to-get-mymonty-under-18-2.webp';
@@ -15,10 +16,14 @@ import Image from 'next/image';
 
 const CardsSlider = () => {
     const swiperParams = {
-        modules:[EffectFade],
+        modules:[EffectFade, Autoplay],
         slidesPerView: 1,
         spaceBetween: 20,
         grabCursor: true,
+        autoplay: {
+            delay: 2000,  // This will be too fast, likely not what you want
+            disableOnInteraction: false,
+        },
         effect: 'fade',
         fadeEffect: {
             crossFade: true,
--- app/mymonty-under-18/page.js
+++ app/mymonty-under-18/page.js
@@ -33,7 +33,7 @@ const Under18 = () => {
             <section className='flex flex-col relative lg:min-h-[100dvh] pt-[var(--header-height)] overflow-hidden max-lg:pb-32'>
                 <img src='/youth-banner.webp' alt='Youth Card' className='absolute top-0 left-0 object-cover object-bottom w-full h-full -z-10' />
 
-                <div className='container h-full text-center mt-20 lg:mt-40'>
+                <div className='container h-full text-center mt-20 lg:mt-20 xl:mt-40'>
                     <h1 className='font-black text-[42px] ssm:text-5xl md:text-6xl lg:text-[5.4rem] xl:text-[5.4rem] mt-6 leading-none uppercase'>Be life ready</h1>
                     <p className='text-xl mt-8'>Your money. Your rules. Your Card.</p>
                     <button className='mm-button !bg-white !text-black mt-8'>Download The App</button>
