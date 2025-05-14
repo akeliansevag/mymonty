e747474843b61f044da7d13e0851b1c496408e14
Hassan Al Jammal <hassan-jammal@hotmail.com>
2025-05-14 16:59:10 +0300

    Adjust Under18 component layout by modifying margin and image width for better responsiveness

--- app/mymonty-under-18/page.js
+++ app/mymonty-under-18/page.js
@@ -33,11 +33,11 @@ const Under18 = () => {
             <section className='flex flex-col relative lg:min-h-[100dvh] pt-[var(--header-height)] overflow-hidden max-lg:pb-32'>
                 <img src='/youth-banner.webp' alt='Youth Card' className='absolute top-0 left-0 object-cover object-bottom w-full h-full -z-10' />
 
-                <div className='container h-full text-center mt-20 lg:mt-20 xl:mt-40'>
+                <div className='container h-full text-center mt-20 2xl:mt-40'>
                     <h1 className='font-black text-[42px] ssm:text-5xl md:text-6xl lg:text-[5.4rem] xl:text-[5.4rem] mt-6 leading-none uppercase'>Be life ready</h1>
                     <p className='text-xl mt-8'>Your money. Your rules. Your Card.</p>
                     <button className='mm-button !bg-white !text-black mt-8'>Download The App</button>
-                    <Image quality={100} sizes='100vw' priority placeholder='blur' alt='Youth Card' src={youthCard} className="lg:w-1/2 mx-auto mt-20" />
+                    <Image quality={100} sizes='100vw' priority placeholder='blur' alt='Youth Card' src={youthCard} className="w-[40%] 2xl:w-1/2 mx-auto mt-10 2xl:mt-20" />
                 </div>
             </section>
 
