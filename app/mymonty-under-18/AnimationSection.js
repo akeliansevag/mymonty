18e77567740344872838494ab711366199ad7914
Hassan Al Jammal <hassan-jammal@hotmail.com>
2025-05-15 14:19:56 +0300

    .

--- app/mymonty-under-18/AnimationSection.js
+++ app/mymonty-under-18/AnimationSection.js
@@ -10,25 +10,25 @@ export default function AnimationSection() {
     const sectionRef = useRef(null); // Only one sectionRef here
     const cardRefs = useRef([]);
 
-    useEffect(() => {
-        // Make sure cards are available in the ref before initializing animation
-        if (cardRefs.current.length) {
-            gsap.from(cardRefs.current, {
-                scrollTrigger: {
-                    trigger: sectionRef.current,
-                    start: "top top", // Pin from the start of the section
-                    end: "+=100%", // Set scrollable range to 100% of the section height
-                    scrub: true,
-                    pin: true, // Pin the section while scrolling
-                    anticipatePin: 1, // Smooth pinning behavior
-                },
-                x: "300%", // Cards enter from the right (off-screen)
-                rotation: (index) => (index % 2 === 0 ? -5 : 5), // Slight rotation for each card
-                opacity: 1,
-                stagger: 0.3,
-            });
-        }
-    }, []);
+    // useEffect(() => {
+    //     // Make sure cards are available in the ref before initializing animation
+    //     if (cardRefs.current.length) {
+    //         gsap.from(cardRefs.current, {
+    //             scrollTrigger: {
+    //                 trigger: sectionRef.current,
+    //                 start: "top top", // Pin from the start of the section
+    //                 end: "+=100%", // Set scrollable range to 100% of the section height
+    //                 scrub: true,
+    //                 // pin: true, // Pin the section while scrolling
+    //                 anticipatePin: 1, // Smooth pinning behavior
+    //             },
+    //             x: "300%", // Cards enter from the right (off-screen)
+    //             rotation: (index) => (index % 2 === 0 ? -5 : 5), // Slight rotation for each card
+    //             opacity: 1,
+    //             stagger: 0.3,
+    //         });
+    //     }
+    // }, []);
 
     // Card data
     const cards = [
@@ -39,7 +39,7 @@ export default function AnimationSection() {
     ];
 
     return (
-        <section className="h-[100vh] lg:h-[100vh] flex flex-col lg:justify-center relative pt-40 lg:pt-20 lg:pb-20 bg-[#FF7300] text-white overflow-hidden" ref={sectionRef}>
+        <section className="lg:h-[100vh] flex flex-col lg:justify-center relative py-20 bg-[#FF7300] text-white overflow-hidden" ref={sectionRef}>
             <img src="/shape-11.svg" alt="Shape" className="absolute top-0 left-0 w-full z-10" />
             <img src="/shape-12.svg" alt="Shape" className="absolute top-0 left-0" />
             <div className="container relative">
@@ -62,13 +62,13 @@ export default function AnimationSection() {
 
                     {/* Right section with cards */}
                     <div className="lg:w-2/3 relative">
-                        <img src="/browser.svg" alt="Browser" className="absolute lg:-top-20 lg:left-32 max-lg:w-full" />
+                        <img src="/browser.svg" alt="Browser" className="absolute top-0 xl:-top-20 lg:left-32 max-lg:w-full" />
 
                         {cards.map((card, i) => (
                             <div
                                 key={i}
                                 ref={(el) => (cardRefs.current[i] = el)}
-                                className="tca-card flex items-center justify-center mb-8 max-lg:mt-10 absolute top-0 left-0 lg:left-1/2 lg:-translate-x-1/2"
+                                className="tca-card flex max-lg:flex-col items-center justify-center mb-8 max-lg:mt-10 lg:absolute lg:top-0 lg:left-0 lg:left-1/2 lg:-translate-x-1/2"
                                 style={{
                                     rotate: `${
                                         i % 4 === 0 ? -5 :
--- app/mymonty-under-18/page.js
+++ app/mymonty-under-18/page.js
@@ -34,10 +34,10 @@ const Under18 = () => {
                 <img src='/youth-banner.webp' alt='Youth Card' className='absolute top-0 left-0 object-cover object-bottom w-full h-full -z-10' />
 
                 <div className='container h-full text-center mt-20 2xl:mt-40'>
-                    <h1 className='font-black text-[42px] ssm:text-5xl md:text-6xl lg:text-[5.4rem] xl:text-[5.4rem] mt-6 leading-none uppercase'>Be life ready</h1>
+                    <h1 className='font-black text-[42px] sm:text-5xl md:text-6xl lg:text-[5.4rem] xl:text-[5.4rem] mt-6 leading-none uppercase'>Be life ready</h1>
                     <p className='text-xl mt-8'>Your money. Your rules. Your Card.</p>
                     <button className='mm-button !bg-white !text-black mt-8'>Download The App</button>
-                    <Image quality={100} sizes='100vw' priority placeholder='blur' alt='Youth Card' src={youthCard} className="w-[40%] 2xl:w-1/2 mx-auto mt-10 2xl:mt-20" />
+                    <Image quality={100} sizes='100vw' priority placeholder='blur' alt='Youth Card' src={youthCard} className="w-[100%] lg:w-[40%] 2xl:w-1/2 mx-auto mt-10 2xl:mt-20" />
                 </div>
             </section>
 
