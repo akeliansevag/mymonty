3dc72c2e6f4e72b2f48878fffbbeb5980f835ab4
akeliansevag <akelian.sevag@gmail.com>
2025-05-19 10:44:08 +0300

    .

--- app/components/NakhalBannerAd.js
+++ app/components/NakhalBannerAd.js
@@ -8,7 +8,7 @@ import nakhalmobile from '@/public/nakhal-mobile.webp';
 
 const KhouryHome = () => {
   return (
-    <Link href="/nakhal" className="fixed z-20 bottom-0 w-full">
+    <Link href="/nakhal" className="fixed z-30 bottom-0 w-full">
       <Image className='max-sm:hidden w-full' quality={100} sizes='100vw' priority placeholder='blur' alt='Nakhal Banner' src={nakhaldesktop} />
       <Image className='sm:hidden w-full' quality={100} sizes='100vw' priority placeholder='blur' alt='Nakhal Banner' src={nakhalmobile} />
     </Link>
