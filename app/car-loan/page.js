4e01d326e5b17a672aad26ceef955b440e98d1f1
Hassan Al Jammal <hassan-jammal@hotmail.com>
2025-07-02 09:56:17 +0300

    .

--- app/car-loan/page.js
+++ app/car-loan/page.js
@@ -108,7 +108,7 @@ const CarLoan = () => {
                       <path d="M7.5 12L10.5 15L16.5 9M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z" stroke="white" strokeOpacity="0.4" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/>
                     </svg>              
                     Earn a net monthly income of $1,000+ (single) or $1,500+ (married)
-                  </li>strokeLinecap
+                  </li>
                   <li className='flex gap-2'>
                     <svg className="flex-shrink-0" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                       <path d="M7.5 12L10.5 15L16.5 9M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z" stroke="white" strokeOpacity="0.4" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"/>
