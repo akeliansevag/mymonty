4f1997cc36a283abab0e078427923afc600b2871
akeliansevag <akelian.sevag@gmail.com>
2025-08-20 10:06:46 +0300

    .

--- app/blogs/[slug]/page.js
+++ app/blogs/[slug]/page.js
@@ -1,43 +1,44 @@
-// import BlogDetails from '@/app/components/BlogDetails';
-// import React from 'react';
-// import { BLOGS_API } from '@/app/api/api';
-// import { websiteName } from '@/app/config';
+import BlogDetails from '@/app/components/BlogDetails';
+import React from 'react';
+import { BLOGS_API } from '@/app/api/api';
+import { websiteName } from '@/app/config';
 
 export async function generateMetadata({ params }) {
-    // const res = await fetch(`${BLOGS_API}/${params.slug}`);
-    // const jsonData = await res.json();
+    const res = await fetch(`${BLOGS_API}/${params.slug}`);
+    const jsonData = await res.json();
 
-    // if (!jsonData.data || jsonData.data.length === 0) {
-    //     return {
-    //         title: "Blog Not Found",
-    //         description: "The blog article you're looking for is not available.",
-    //     };
-    // }
+    if (!jsonData.data || jsonData.data.length === 0) {
+        return {
+            title: "Blog Not Found",
+            description: "The blog article you're looking for is not available.",
+        };
+    }
 
-    // const blogItem = jsonData.data[0];
+    const blogItem = jsonData.data[0];
 
-    // return {
-    //     title: websiteName + blogItem.title,
-    //     description: websiteName + blogItem.description.replace(/<[^>]*>?/gm, ''), // Remove HTML tags
-    //     openGraph: {
-    //         title: websiteName + blogItem.title,
-    //         description: websiteName + blogItem.description.replace(/<[^>]*>?/gm, ''),
-    //         images: [
-    //             {
-    //                 url: blogItem.image,
-    //                 width: 1200,
-    //                 height: 900,
-    //                 alt: blogItem.title,
-    //             },
-    //         ],
-    //     },
-    // };
+    return {
+        title: websiteName + blogItem.title,
+        description: websiteName + blogItem.description.replace(/<[^>]*>?/gm, ''), // Remove HTML tags
+        openGraph: {
+            title: websiteName + blogItem.title,
+            description: websiteName + blogItem.description.replace(/<[^>]*>?/gm, ''),
+            images: [
+                {
+                    url: blogItem.image,
+                    width: 1200,
+                    height: 900,
+                    alt: blogItem.title,
+                },
+            ],
+        },
+    };
 }
 
 const page = ({params}) => {
   return (
-    <></>
-    // <BlogDetails params={params} />
+    <>
+      <BlogDetails params={params} />
+    </>
   );
 }
 
