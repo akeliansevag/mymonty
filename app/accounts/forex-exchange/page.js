de05a6207a87d476054921be6ca9cc7e52950a27
akeliansevag <akelian.sevag@gmail.com>
2025-12-02 11:56:26 +0200

    ,

---
+++ app/accounts/forex-exchange/page.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import { websiteName } from '@/app/config';
+import Banner from '@/app/components/Banner';
+import banner from '@/public/forex-exchange-banner.webp';
+import TextBlock from '@/app/components/TextBlock';
+import Branches from '@/app/components/Branches';
+import ATMs from '@/app/components/ATMs';
+
+export const metadata = {
+    title: 'Forex Exchange' + websiteName,
+    description: 'Exchange on the go',
+};
+const page = () => {
+    const data = {
+        title: 'Exchange on the go',
+        subtitle: 'Accounts',
+        desc: 'Quick. Fair. Hassle-free.',
+        image: {
+            inGrid: true,
+            layout: 'half',
+            url: banner,
+        },
+        cta: {
+            type: 'button',
+            button: {
+                name: 'Download app'
+            }
+        }
+    };
+
+    const terms = {
+        termsAndConditions: false, 
+        kfs: 'KFS-Individual-eWallet-Account-16.7.25.pdf'
+    }
+
+  return (
+    <>
+        <Banner data={data} terms={terms}/>
+        <section className='max-lg:py-14 lg:py-28 bg-white'>
+            <div className='container text-center'>
+                <TextBlock title="Switch Between USD and LBP" description="Get the daily market rate instantly. <br /> No queues. No Waiting. It’s all on your app." center={true} />
+                <img src='/switch-between.webp' className='w-full mt-16'/>
+            </div>
+        </section>
+
+        <section className="bg-white py-28 max-lg:py-14">
+            <div className='container'>
+                <div className='flex flex-col gap-20'>
+                    <div className='flex items-center max-lg:gap-10 lg:gap-32 max-lg:flex-col'>
+                        <div className='w-full'>
+                            <TextBlock buttonColor="dark" title="Fast AND Transparent" description="See exactly what you get before you confirm. Your money, your control." />
+                        </div>
+                        <div className='w-full'>
+                            <img src='/fast-and-transparent.webp' className='max-w-[575px] w-full' alt='Forex Image'/>
+                        </div>
+                        
+                    </div>
+
+                    <div className='flex items-center max-lg:gap-10 lg:gap-32 max-lg:flex-col-reverse'>
+                        <div className='w-full'>
+                            <img src='/safe-and-secure.webp' alt='Statement Image' className='max-w-[575px] w-full'/>
+                        </div>
+                        <div className='w-full'>
+                            <TextBlock buttonColor="dark" title="Safe AND Secure" description="All transactions are protected by MyMonty’s top-notch security, so you can exchange with confidence."/>
+                        </div>
+                        
+                        
+                    </div>
+
+                    <div className='flex items-center max-lg:gap-10 lg:gap-32 max-lg:flex-col'>
+                        <div className='w-full'>
+                            <TextBlock buttonColor="dark" title="Exchange money like a pro" description="Skip the exchange office and forget asking friends or colleagues for change. Your wallet handles it all." />
+                        </div>
+                        <div className='w-full'>
+                            <img src='/exchange-money.webp' className='max-w-[575px] w-full' alt='Forex Image'/>
+                        </div>
+                        
+                    </div>
+
+                </div>
+                
+            </div>
+        </section>
+        <section>
+            <div className='container text-center lg:mb-28 max-lg:mb-14'>
+                <TextBlock buttonColor="dark" center={true} title="Powered by Srour Company for Exchange S.A.R.L." button={true}/>
+            </div>
+        </section>
+    </>
+  )
+}
+
+export default page;
---
+++ app/accounts/withdraw/page.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { websiteName } from '@/app/config';
+import Banner from '@/app/components/Banner';
+import banner from '@/public/withdraw-banner.webp';
+import TextBlock from '@/app/components/TextBlock';
+import Branches from '@/app/components/Branches';
+import ATMs from '@/app/components/ATMs';
+
+export const metadata = {
+    title: 'Withdraw' + websiteName,
+    description: 'Running out of cash?',
+};
+const page = () => {
+    const data = {
+        title: 'Running out of cash?',
+        subtitle: 'Accounts',
+        desc: 'MyMonty is Everywhere.',
+        image: {
+            inGrid: true,
+            layout: 'half',
+            url: banner,
+        },
+        cta: {
+            type: 'button',
+            button: {
+                name: 'Download app'
+            }
+        }
+    };
+
+    const terms = {
+        termsAndConditions: false, 
+        kfs: 'KFS-Individual-eWallet-Account-16.7.25.pdf'
+    }
+
+  return (
+    <>
+        <Banner data={data} terms={terms}/>
+        <Branches title="Withdraw via MyMonty Branch" description="We’d love to meet you! Visit us at:" info="Our staff at the counter are always ready to support you!" />
+        <section className='max-lg:py-14 lg:py-28 bg-white'>
+            <div className='container text-center'>
+                <TextBlock title="Withdraw via Agent Partners" description="Cash out anywhere, anytime. <br /> Visit any of the <strong>1,400+ OMT locations </strong> across Lebanon. Give them your ID transaction number, grab your cash, and watch your wallet balance adjust instantly." center={true} />
+                <img src='/top-up-via-agent-partners.webp' className='w-full mt-16'/>
+            </div>
+        </section>
+
+        <ATMs title="Withdraw via MyMonty ATMs" description="MyMonty is there, wherever you need it! <br /> Unload your wallet balance without needing an ID or even a card. <br /> Our ATMs are available at the following locations:" bgColor="#D25788" />
+
+        <section className="bg-white py-28 max-lg:py-14">
+            <div className='container'>
+                <div className='flex flex-col gap-20'>
+                    <div className='flex items-center max-lg:gap-10 lg:gap-32 max-lg:flex-col'>
+                        <div className='w-full'>
+                            <TextBlock buttonColor="dark" title="Withdraw via Integrated ATMs" description="Contactless withdrawals, a little magic that puts cash in your hands. <br /> Head to your nearest Fransabank or BLC ATM, select MyMonty, follow the steps, and voilà, your money is ready. <br /> Don’t forget your phone… who would anyway? " link="/accounts/top-up" />
+                        </div>
+                        <div className='w-full'>
+                            <img src='/top-up-via-integrated-atms.webp' className='max-w-[575px] w-full' alt='Forex Image'/>
+                        </div>
+                        
+                    </div>
+
+                </div>
+                
+            </div>
+        </section>       
+    </>
+  )
+}
+
+export default page;
