8439f47243fec23521cd6113182fe3ff0a11cbe1
akeliansevag <akelian.sevag@gmail.com>
2026-01-23 14:04:19 +0200

    .

--- app/homepage/SectionFive.js
+++ app/homepage/SectionFive.js
@@ -8,13 +8,11 @@ const SectionFive = () => {
     const { handleOpenModal } = useAppContext();
     
     return (
-        <section
-            className="relative min-h-[calc(80vh-var(--mobile-header-height))] lg:min-h-[calc(100vh-var(--mobile-header-height))] flex justify-center overflow-hidden py-14"
-        >
-            <img className="absolute bottom-0 left-0 w-full h-full object-cover" src='/section5bg.webp' width="" height="" />
+        <section className="relative overflow-hidden">
+            <img className="w-full" src='/section5bg.webp' width="" height="" />
 
             {/* Content */}
-            <div className="container relative z-10 text-center px-4">
+            <div className="container absolute z-10 left-1/2 top-16 -translate-x-1/2 text-center px-4">
                 <div className='lg:w-1/3 mx-auto'>
                     <h2 className="uppercase font-black text-3xl sm:text-5xl md:text-[2.875rem] leading-none">
                         prepaid Cards 
